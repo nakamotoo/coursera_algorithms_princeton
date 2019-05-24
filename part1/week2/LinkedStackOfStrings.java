4455c7473c3f01fc137e771aca03a2606a3f28bf
nakamotoo <mitsuhiko0820.acm@gmail.com>
2019-05-24 13:10:14 +0900

    stack linked list

---
+++ part1/week2/LinkedStackOfStrings.java
@@ -0,0 +1,38 @@
+public class LinkedStackOfStrings {
+    private Node first = null;
+
+    private class Node {
+        String item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public void push(String item) {
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public String pop() {
+        String item = first.item;
+        first = first.next;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        System.out.print(stack.isEmpty() + "\n");
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        System.out.print(stack.isEmpty() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.isEmpty() + "\n");
+    }
+}
