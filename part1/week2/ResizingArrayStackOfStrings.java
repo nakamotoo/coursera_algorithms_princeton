36827158fccce4c3fba0d8621b5851973e52a1ee
nakamotoo <mitsuhiko0820.acm@gmail.com>
2019-05-24 23:23:10 +0900

    resize array stack push

---
+++ part1/week2/ResizingArrayStackOfStrings.java
@@ -0,0 +1,51 @@
+// FixedCapacityArrayStackOfStringsの改善版
+
+public class ResizingArrayStackOfStrings {
+
+    private String[] s;
+    private int N = 0;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1]; // capacityの指定がいらなくなって、最初はサイズ１の配列を作ればよい！
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
+    }
+
+    public void push(String item) {
+        // 配列が満タンになったらサイズを２倍に拡張する
+        if (N == s.length)
+            resize(2 * s.length);
+        s[N++] = item;
+    }
+
+    public String pop() {
+        String item = s[--N];
+        S[N] = null;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        FixedCapacityArrayStackOfStrings stack = new FixedCapacityArrayStackOfStrings(n);
+        System.out.print(stack.isEmpty() + "\n");
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        System.out.print(stack.isEmpty() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.pop() + "\n");
+        System.out.print(stack.isEmpty() + "\n");
+    }
+}
