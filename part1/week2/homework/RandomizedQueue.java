55c93b376e1eccc8bb5e421f326162f897302d5f
nakamoto <mitsuhiko0820.acm@gmail.com>
2019-06-11 21:25:02 +0900

    randomized queue

---
+++ part1/week2/homework/RandomizedQueue.java
@@ -0,0 +1,87 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable <Item> {
+
+  private Item[] s;
+  private int N = 0;
+
+  // construct an empty randomized queue
+  public RandomizedQueue(){
+    s = (Item[]) new Object[1];
+  }
+
+  // is the randomized queue empty?
+  public boolean isEmpty(){
+    return N == 0;
+  }
+
+  //return the number of items on the randomized queue
+  public int size(){
+    return N;
+  }
+
+  // resize array
+  private void resize(int capacity){
+    Item[] copy = new Item[capacity];
+    for(int i = 0; i < N; i++){
+      copy[i] = s[i];
+    }
+    s = copy;
+  }
+
+  // add the item
+  public void enqueue(Item item){
+    if(N == s.length)
+      resize(2 * s.length);
+    s[N++] = item;
+  }
+
+  // remove and return a random item
+  public Item dequeue(){
+    int n = StdRandom.uniform(N);
+    Item[] new_s = new Item[s.length];
+    for(int i = 0; i < n; i++){
+      new_s[i] = s[i];
+    }
+    if(n < s.length - 1){
+        for(int j = n+1; j < N; j++ ){
+            new_s[j-1] = s[j];
+        }
+    }
+    Item rtn = s[n];
+    s = new_s;
+    return rtn;
+  }
+
+  // return a random item(but do not remove it)
+  public Item sample(){
+    int n = StdRandom.uniform(N);
+    return s[n];
+  }
+
+  // return an independent iterator over items in random order
+  public Iterator<Item> iterator(){
+    return new ArrayIterator();
+  }
+
+  private class ArrayIterator implements Iterator<Item>{
+    private int i = N;
+    private Item[] copy = new Item[s.length];
+    copy = StdRandom.uniform(s);
+
+    public boolean hasNext(){return i > 0; }
+    public void remove(){
+        throw new java.lang.UnsupportedOperationException("remove() is unsupported...");
+    }
+    public Item next(){
+      return copy[--i];
+    }
+  }
+
+  public static void main(String[] args){
+
+  }
+}
