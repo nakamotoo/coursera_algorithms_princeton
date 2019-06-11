dca9d23545fcb6ee06361f567ecebd2366e163f0
nakamotoo <mitsuhiko0820.acm@gmail.com>
2019-06-11 13:11:08 +0900

    finish permutation.java

--- part1/week2/homework/Permutation.java
+++ part1/week2/homework/Permutation.java
@@ -1,8 +1,17 @@
 import edu.princeton.cs.algs4.StdIn;
 
-public class Permutation{
-  public static void main(String[] args){
-    String a = StdIn.readString();
-    System.out.print(a);
-  }
+public class Permutation {
+
+    public static void main(String[] args) {
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+        int k = Integer.parseInt(args[0]);
+        while (!StdIn.isEmpty()) {
+            String a = StdIn.readString();
+            q.enqueue(a);
+        }
+        for (int i = 0; i < k; i++) {
+            String rtn = q.dequeue();
+            System.out.print(rtn + "\n");
+        }
+    }
 }
